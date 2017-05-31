0824a0af85b0a610cc47cfc45845ad8910082d6f
ThiagoMoratori <thiagomoratori@gmail.com>
2017-05-30 21:49:35 -0300

    Second Commit

---
+++ src/swinfo.js
@@ -0,0 +1,16 @@
+/**
+ * Created by AppMasters on 30/05/2017.
+ */
+import React from 'react';
+
+class SWInfo extends React.Component{
+    render(){
+        return(
+            <div>
+                Thiago
+            </div>
+        )
+    }
+}
+
+export default SWInfo;
